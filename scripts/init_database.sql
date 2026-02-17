c36d9994900db6a08f5d576501743068b7d83383
Devis-D <devisdoci01@gmail.com>
2026-02-17 08:57:07 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,12 @@
+-- Here we are creating our new Database.
+CREATE DATABASE DataWareHouse;
+--Making sure that the database that we are currently using its the new one.
+use DataWareHouse
+
+-- After creating our database now we create schemas or the Medallion Architecture
+CREATE SCHEMA SILVER;
+GO
+CREATE SCHEMA GOLD;
+GO
+
 
